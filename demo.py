a7950d2352f2afba59d9d864c622bb6afcf161e6
Seth Stenzel <seth.stenzel@nxlog.org>
2022-08-02 03:13:51 -0500

    Small changes to correct issues with both demo.py and display_pbm.py, added disptime to display_image.

--- demo.py
+++ demo.py
@@ -4,16 +4,17 @@ import time
 hw_pin_scl, hw_pin_sda = (4 , 5)
 i2c, lcd = setup_display(hw_pin_scl, hw_pin_sda)
 
+while True:
 # SINGLE IMAGE DEMO
-display_image(lcd, "./img", "ash-pikachu.pbm", True)
-time.sleep(2)
+  display_image(lcd, "./img", "ash-pikachu.pbm", True)
+  time.sleep(2)
 
-# SLIDE SHOW DEMO
-display_slide_show(lcd, "./img", ["mario-kart.pbm", "pokemon.pbm", "tetris.pbm", "three-starters.pbm"], True, 2000, 1)
+  # SLIDE SHOW DEMO
+  display_slide_show(lcd, "./img", ["mario-kart.pbm", "pokemon.pbm", "tetris.pbm", "three-starters.pbm"], True, 2000, 1)
 
-# ANIMATION DEMO
-display_animation(lcd, "./ani/mario_dance", True, 66, 23, 6, 1)
-display_animation(lcd, "./ani/streetf", True, 33, 38, 15, 1)
-display_animation(lcd, "./ani/yoshi_walk", True, 99,  5, 10, 1)
+  # ANIMATION DEMO
+  display_animation(lcd, "./ani/mario_dance", True, 66, 23, 6, 1)
+  display_animation(lcd, "./ani/streetf", True, 33, 38, 15, 1)
+  display_animation(lcd, "./ani/yoshi_walk", True, 99,  5, 10, 1)
 
 
